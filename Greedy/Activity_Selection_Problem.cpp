60061bf4a32472175ac89cea38e86c305be88cfc
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2022-06-17 07:14:19 +0600

    Update Activity_Selection_Problem.cpp

--- Greedy/Activity_Selection_Problem.cpp
+++ Greedy/Activity_Selection_Problem.cpp
@@ -1,6 +1,7 @@
 /*
 Also known as scheduling problem
-You are given n activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
+You are given n activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person
+can only work on a single activity at a time.
 */
 // we can easily solve this problem using Finish Time of each activity
 
