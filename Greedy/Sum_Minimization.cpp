bf8188f4f4c5852ed0d1cd1d5ed9a9c5ca1d1b30
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2022-07-17 21:40:43 +0600

    Update Sum_Minimization.cpp

--- Greedy/Sum_Minimization.cpp
+++ Greedy/Sum_Minimization.cpp
@@ -3,4 +3,4 @@ Imagine You have an array A. You have to find an integer x such that
 |a1 − x| + |a2 − x| + |a3 − x|... + |an − x| is minimum possible
 */
 
-//sort the array and miffle element is the answer...BOOOOM TADATCADACatadaskdsk
+//sort the array and middle element is the answer...BOOOOM TADATCADACatadaskdsk
