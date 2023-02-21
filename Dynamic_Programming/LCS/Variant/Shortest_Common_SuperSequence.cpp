89726d97e9b442c80c27e2f3d2d8ab3b7f41d390
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2023-02-21 08:40:45 +0600

    Update Shortest_Common_SuperSequence.cpp

--- Dynamic_Programming/LCS/Variant/Shortest_Common_SuperSequence.cpp
+++ Dynamic_Programming/LCS/Variant/Shortest_Common_SuperSequence.cpp
@@ -52,3 +52,58 @@ GXTXAYB
 Shortest common supersequence size length = 9
 AGGXTXAYB
 */
+
+
+
+//Priting the shortest common supersequence string
+ int n = str1.size(), m = str2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]= 1 + dp[i-1][j-1]; 
+                }
+                else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    //Got My LCS Length;
+
+    //Now it's time to get supersequence string
+
+    string ans = "";
+
+    int i = n, j = m;
+    while(i>0 && j>0){
+        if(str1[i-1]==str2[j-1]){
+            ans+=str1[i-1];
+            i--;j--;
+        }
+        else if(dp[i-1][j]>dp[i][j-1]){
+            ans+=str1[i-1];
+            i--;
+        }
+        else
+        {
+            ans+=str2[j-1];
+            j--;
+        }
+    }
+    while(i>0){
+        ans+=str1[i-1];
+        i--;
+    }
+    while(j>0){
+        ans+=str2[j-1];
+        j--;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+
